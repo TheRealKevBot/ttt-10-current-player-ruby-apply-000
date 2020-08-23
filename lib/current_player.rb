4b3c19aefeea6a86c6e96474736fa9d1f4779cfb
Kevin Glasgow <kglas001@gmail.com>
2020-08-23 18:46:21 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,9 +8,19 @@ def turn_count(board)
   return counter
 end
 
-def current_player(board)
-  if 
+#def current_player(board)
+ #if 
   
-  else
+  #else
   
-end
+#end 
+
+#def turn_count(board)
+ #counter = 0  
+  #board.each do |move|
+    #if move == "X" || move == "O"
+      #counter =+ 1
+    #end
+  #return counter
+  #end
+#end
