95646b2efdf1c6e7ff4e66771f620d4366673809
Kevin Glasgow <kglas001@gmail.com>
2020-08-23 18:12:13 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def turn_count(board)
+  counter = 0 
+  board.each do |move|
+    if (move == "X" || move == "O")
+      counter =+ 1
+    end
+  return counter
+  end
+end
+
+def current_player()
+  
+end
